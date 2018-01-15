751006c0ccdac62699687cb2bc43d14d68153a61
jano744 <jan.pokryvka2206@centrum.sk>
2018-01-15 10:09:04 +0100

    onClick

--- app/src/main/java/com/z/planner/MainActivity.java
+++ app/src/main/java/com/z/planner/MainActivity.java
@@ -2,6 +2,9 @@ package com.z.planner;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+    }
+
+    public void onClick(View view) {
+        CalendarView calendar = (CalendarView) findViewById(R.id.calendarView);
+        EditText editText = (EditText) view;
+        calendar.setVisibility(View.GONE);
     }
 }
